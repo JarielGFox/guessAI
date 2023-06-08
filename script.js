faecf4fbb5a8cbc095b5364488efeaeac3f8f41f
Giovanni Crudele <78105764+JarielGFox@users.noreply.github.com>
2023-06-08 15:10:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const API_URL = "https://api.openai.com/v1/chat/completions";
 const MODEL = "gpt-3.5-turbo";
 
 // Inserisci qui la tua chiave API
-const API_KEY = "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";
+const API_KEY = "";
 
 const loader = document.querySelector('.loading');
 const modal = document.querySelector(".modal");
